981bc020297751e9b1a2605e99237ec8d77adb20
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-10-04 15:28:18 +0200

    Add some clarifying comments

--- cities.php
+++ cities.php
@@ -8,6 +8,9 @@ foreach ($teams as $teamName => $teamData) {
 
 $uniqueCities = array_unique($cities);
 $lastCity = array_pop($uniqueCities);
+// The last item (city) in the array is taken out
+// in order to swap out "," to "and" in the <div> below.
+// like this: "Göteborg, Malmö, Uppsala AND Stockholm"
 
 ?>
 
